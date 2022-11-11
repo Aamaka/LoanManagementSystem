e748b24ce343ae14a238277b4605e270dcbf2d68
Chiamaka Ezeirunne <fayhvor@gmail.com>
2022-11-11 11:54:38 +0100

    loan

--- src/main/java/ezeirunne/chiamaka/loanmanagementsystem/security/provider/LoanAuthenticationProvider.java
+++ src/main/java/ezeirunne/chiamaka/loanmanagementsystem/security/provider/LoanAuthenticationProvider.java
@@ -3,10 +3,12 @@ package ezeirunne.chiamaka.loanmanagementsystem.security.provider;
 import ezeirunne.chiamaka.loanmanagementsystem.util.LoanUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -23,17 +25,22 @@ public class LoanAuthenticationProvider implements AuthenticationProvider {
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         UserDetails userDetails = userDetailService.loadUserByUsername((String) authentication.getPrincipal());
         if(userDetails != null){
-            if(passwordEncoder.matches((String) authentication.getCredentials(), userDetails.getPassword())){
-                UsernamePasswordAuthenticationToken authenticationToken =
-                        new UsernamePasswordAuthenticationToken(userDetails, authentication.getCredentials(), authentication.getAuthorities());
+            if(isMatches(authentication, userDetails)){
+                return new UsernamePasswordAuthenticationToken(userDetails, authentication.getCredentials(),
+                        authentication.getAuthorities());
             }
+            throw new BadCredentialsException("Incorrect password");
         }
+        throw new UsernameNotFoundException("Email not found");
+    }
 
-        return null;
+    private boolean isMatches(Authentication authentication, UserDetails userDetails) {
+        return passwordEncoder.matches((String) authentication.getCredentials(), userDetails.getPassword());
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return false;
+        Class<UsernamePasswordAuthenticationToken> appAuthType = UsernamePasswordAuthenticationToken.class;
+        return authentication.equals(appAuthType);
     }
 }
